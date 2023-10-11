35093af3cd5fef0c8bf1de8675c69cf83566d3c0
wfa22 <alx.bod@mail.ru>
2023-10-11 11:30:46 +0300

    Lab 1

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+import wikipedia
+from pydantic import BaseModel
+#uvicorn main:app --reload
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Wikiinput(BaseModel):
+    """Класс для ввода названия сводки и кол-ва предложений"""
+    s : str
+    cnt : int
+class Wikioutput(BaseModel):
+    """Класс для вывода сводки"""
+    output : str
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+wikipedia.set_lang("ru")
+app = FastAPI()
+
+@app.get("/{search}", response_model=Wikioutput)
+def src(search : str):
+    """Возвращает сводку по вашему запросу через path"""
+    res= wikipedia.summary(search, sentences=10)
+    return Wikioutput(output=res)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/next/query", response_model=Wikioutput)
+def with_query(search:str, number:int):
+    """Возвращает указанное кол-во предложений из сводки по вашему запросу через query"""
+    res = wikipedia.summary(search, sentences=number)
+    return Wikioutput(output=res)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.post("/classes", response_model=Wikioutput)
+def classs(src: Wikiinput):
+    """Возвращает указанное кол-во предложений из сводки по вашему запросу"""
+    res = f'Запрашиваемое резюме: {wikipedia.summary(src.s, sentences= src.cnt)}'
+    return Wikioutput(output=res)
